423929fb17f20709279ef52ceec142cf56967d32
Mart7anova <mart7anova@gmail.com>
2022-11-11 16:16:10 +0300

    feat: request and add users

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+
+type UsersType = Array<{
+    id: number
+    name: string
+}>
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState<UsersType>([])
+
+    const getUsers = () => {
+        axios.get('http://localhost:7542/users')
+            .then(res => setUsers(res.data))
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
+    const crateUser = () => {
+      axios.post('http://localhost:7542/users')
+          .then(() =>  getUsers())
+    }
+
+    return (
+        <div className="App">
+            <button onClick={crateUser}>Create user</button>
+            {
+                users.map(user => <div key={user.id}>{user.name}</div>)
+            }
+        </div>
+    );
 }
 
 export default App;
