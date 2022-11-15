7e8160bbbd0e64125ecd51721aa119c7459bb257
Mart7anova <mart7anova@gmail.com>
2022-11-15 17:06:47 +0300

    feat: add search user

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
 import axios from 'axios';
 
 type UsersType = Array<{
-    id: number
+    id: string
     name: string
 }>
 
 function App() {
     const [users, setUsers] = useState<UsersType>([])
+    const [name, setName] = useState('')
+    const [param, setParam] = useState('')
 
-    const getUsers = () => {
-        axios.get('http://localhost:7542/users')
+    const getUsers = (param: string = '') => {
+        axios.get(`http://localhost:7542/users?search=${param}`)
             .then(res => setUsers(res.data))
+    }
+
+    const searchUser = () => {
+        getUsers(param)
     }
 
     useEffect(() => {
         getUsers()
     }, [])
 
-    const crateUser = () => {
-      axios.post('http://localhost:7542/users')
+    const crateUser = (name: string) => {
+      axios.post('http://localhost:7542/users', {name})
           .then(() =>  getUsers())
     }
 
     return (
         <div className="App">
-            <button onClick={crateUser}>Create user</button>
+            <h1>Search user</h1>
+            <input value={param} onChange={(e)=>setParam(e.currentTarget.value)}/>
+            <button onClick={searchUser}>Search</button>
+
+            <h1>Create new user</h1>
+            <input value={name} onChange={(e)=>setName(e.currentTarget.value)}/>
+            <button onClick={()=>crateUser(name)}>Create user</button>
+
             {
                 users.map(user => <div key={user.id}>{user.name}</div>)
             }
